cf4b11bef0f3894d164415dc4d0775a3d9f3d132
kentuckey <taro_uematsu@recruit.co.jp>
2013-08-02 13:47:34 +0900

    Initial Commit

---
+++ test/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by kentuckey on 2013/08/02.
+//  Copyright (c) 2013年 kentuckey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  test
+//
+//  Created by kentuckey on 2013/08/02.
+//  Copyright (c) 2013年 kentuckey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
